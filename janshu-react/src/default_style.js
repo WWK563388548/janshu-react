fbb18bf89473bde20559f44b928e84a1fcef6e29
WWK563388548 <wc563388548@gmail.com>
2019-02-16 11:57:18 +0900

    create global style by styled-components

---
+++ janshu-react/src/default_style.js
@@ -0,0 +1,12 @@
+import {createGlobalStyle} from 'styled-components';
+
+// Create global style of app
+export const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+`
--- janshu-react/src/index.js
+++ janshu-react/src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {GlobalStyle} from './default_style.js'
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const TheApp = () => {
+    return (
+        <Fragment>
+            <GlobalStyle/>
+            <App />
+        </Fragment>
+    );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDOM.render(<TheApp />, document.getElementById('root'));
+
