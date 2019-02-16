9ed308bf017405d209e9b8f36216fc0f73b5c646
WWK563388548 <wc563388548@gmail.com>
2019-02-16 22:25:03 +0900

    update style of header

--- janshu-react/src/components/header/header_style.js
+++ janshu-react/src/components/header/header_style.js
@@ -48,7 +48,7 @@ export const NavSearchField = styled.input.attrs({
 })`
     width: 160px;
     height: 38px;
-    padding: 0 20px;
+    padding: 0 30px 0 20px;
     margin-top: 9px;
     margin-left: 20px;
     box-sizing: border-box;
@@ -57,8 +57,23 @@ export const NavSearchField = styled.input.attrs({
     border-radius: 19px;
     background: #eee;
     font-size: 14px;
+    color: #666;
     &::placeholder{
         color: #999;
+    }
+`;
+
+export const SearchWrapper = styled.div`
+    float: left;
+    position: relative;
+    .iconfont {
+        position: absolute;
+        right: 5px;
+        bottom: 4px;
+        width: 30px;
+        line-height: 30px;
+        border-radius: 15px;
+        text-align: center;
     }
 `;
 
--- janshu-react/src/components/header/index.jsx
+++ janshu-react/src/components/header/index.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {HeaderWrapper, Logo, Nav, NavItem, NavSearchField, AdditionalField, Button} from './header_style.js';
+import {HeaderWrapper, Logo, Nav, NavItem, NavSearchField, SearchWrapper, AdditionalField, Button} from './header_style.js';
 
 class Header extends Component {
     render() {
@@ -25,14 +25,23 @@ class Header extends Component {
                     <NavItem
                         className="right"
                     >
-                        <i className="iconfont">&#xe636;</i>
+                        Aa
                     </NavItem>
-                    <NavSearchField />
-                    <i className="iconfont">&#xe653;</i>
+                    <SearchWrapper>
+                        <NavSearchField />
+                        <i className="iconfont">&#xe653;</i>
+                    </SearchWrapper>
                 </Nav>
                 <AdditionalField>
                     <Button className="writting">
-                        <i className="iconfont">&#xe624;</i>
+                        <i
+                            className="iconfont"
+                            style={{
+                                marginRight: '5px',
+                            }}
+                        >
+                            &#xe624;
+                        </i>
                         写文章
                     </Button>
                     <Button className="reg">注册</Button>
