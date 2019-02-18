eed31a2d0ffafa635ad70a4af7fa262b217cb1e1
WWK563388548 <wc563388548@gmail.com>
2019-02-18 11:35:59 +0900

    add router for any pages

--- janshu-react/src/App.js
+++ janshu-react/src/App.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import Header from "./components/header";
 import store from './store';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Header />
+        <div>
+          <Header />
+          <BrowserRouter>
+            <div>
+              <Route path="/" exact render={() => <div>Home</div>}></Route>
+              <Route path="/detail" exact render={() => <div>Detail</div>}></Route>
+            </div>
+          </BrowserRouter>
+        </div>
       </Provider>
     );
   }
