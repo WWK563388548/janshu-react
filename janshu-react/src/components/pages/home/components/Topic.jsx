7773ad5c6845b41b0c3ca38b840dde38ec55c87f
WWK563388548 <wc563388548@gmail.com>
2019-02-18 13:39:36 +0900

    add style of Topic component

--- janshu-react/src/components/pages/home/components/Topic.jsx
+++ janshu-react/src/components/pages/home/components/Topic.jsx
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import {
+    TopicWrapper,
+    TopicItem,
+    TopicItemMore,
+} from '../home_style';
 
 class Topic extends Component {
     render() {
         return (
-            <div>Topic part</div>
+            <TopicWrapper>
+               <TopicItem>
+                    <img 
+                        className="topic-pic"
+                        src="//upload.jianshu.io/collections/images/345460/u_1240372403_2153081336_fm_58.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/144/h/144/format/webp"
+                        alt="python" 
+                    />          
+                    Python教程/练习题
+               </TopicItem> 
+
+               <TopicItemMore>           
+                    更多热门专题 >
+               </TopicItemMore> 
+            </TopicWrapper>
         );
     }
 }
--- janshu-react/src/components/pages/home/home_style.js
+++ janshu-react/src/components/pages/home/home_style.js
@@ -21,3 +21,41 @@ export const HomeRight = styled.div`
     width: 240px;
     float: right;
 `;
+
+export const TopicWrapper = styled.div`
+    padding: 20px 0 10px 0;
+    overflow: hidden;
+    margin-left: -18px;
+`;
+
+export const TopicItem = styled.div`
+    float: left;
+    height: 32px;
+    line-height: 32px;
+    margin-left: 18px;
+    margin-bottom: 18px;
+    background: #f7f7f7;
+    padding-right: 10px;
+    font-size: 14px;
+    color: #000;
+    border: 1px solid #dcdcdc;
+    border-radius: 4px;
+    .topic-pic{
+        display: block;
+        float: left;
+        width: 32px;
+        height: 32px;
+        margin-right: 10px;
+    }
+`;
+
+export const TopicItemMore = styled.div`
+    float: left;
+    height: 32px;
+    line-height: 32px;
+    margin-left: 18px;
+    margin-bottom: 18px;
+    padding-right: 10px;
+    font-size: 14px;
+    color: #969696;
+`;
