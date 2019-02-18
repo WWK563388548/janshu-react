fbd2fce4df63afd7b79872caca17c41fc9e4278a
WWK563388548 <wc563388548@gmail.com>
2019-02-18 18:10:34 +0900

    add List compoent of home component

--- janshu-react/src/components/pages/home/components/List.jsx
+++ janshu-react/src/components/pages/home/components/List.jsx
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+    ListItem,
+    ListInfo,
+} from '../home_style';
 
 class List extends Component {
     render() {
         return (
-            <div>List part</div>
+            <div>
+                {
+                    this.props.articleList.map((item) => {
+                        return (
+                            <ListItem key={item.get("id")}>
+                                <img
+                                    className="pic"
+                                    src={item.get("image")}
+                                    alt={item.get("title")}
+                                />
+                                <ListInfo>
+                                    <a
+                                        href="#"
+                                        className="title"
+                                    >
+                                        {item.get("title")}
+                                    </a>
+                                    <p className="desc">
+                                        {item.get("desc")}    
+                                    </p>
+                                </ListInfo>
+                            </ListItem>
+                        )
+                    })
+                }
+            </div>
         );
     }
 }
 
-export default List;
+const mapStateToProps = (state) => ({
+    articleList: state.getIn(["home", "articleList"]),
+});
+
+export default connect(mapStateToProps, null)(List);
--- janshu-react/src/components/pages/home/home_style.js
+++ janshu-react/src/components/pages/home/home_style.js
@@ -26,6 +26,7 @@ export const TopicWrapper = styled.div`
     padding: 20px 0 10px 0;
     overflow: hidden;
     margin-left: -18px;
+    border-bottom: 1px solid #dcdcdc
 `;
 
 export const TopicItem = styled.div`
@@ -59,4 +60,33 @@ export const TopicItemMore = styled.a`
     padding-right: 10px;
     font-size: 14px;
     color: #969696;
+`;
+
+export const ListItem = styled.div`
+    overflow: hidden;
+    padding: 20px 0;
+    border-bottom: 1px solid #dcdcdc;
+    .pic{
+        display: block;
+        width: 125px;
+        height: 100px;
+        float: right;
+        border-radius: 10px;
+    }
+`;
+
+export const ListInfo = styled.div`
+    width: 500px;
+    float: left;
+    .title {
+        line-height: 27px;
+        font-size: 18px;
+        font-weight: bold;
+        color: #333;
+    }
+    .desc {
+        line-height: 24px;
+        font-size: 13px;
+        color: #999;
+    }
 `;
